db580802553a87fbb143a7e1af21d580223ece89
Reny Ramos <renyramosgarcia@gmail.com>
2020-11-19 20:41:32 -0400

    First commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\DigimonSimpleSearchController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+ */
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/digimon-simple', DigimonSimpleSearchController::class)->name('search.simple');
+Route::get('/digimon', DigimonSimpleSearchController::class)->name('search');
